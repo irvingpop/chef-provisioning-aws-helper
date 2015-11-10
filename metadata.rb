3334c23db45e5178d94839f07e78c731ce3172d7
Irving Popovetsky <irving@getchef.com>
2015-11-10 09:50:59 -0800

    Add an aws_instance_created method to help out machine precreate steps

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'irving@chef.io'
 license 'all_rights'
 description 'Installs/Configures chef-provisioning-aws-helper'
 long_description 'Installs/Configures chef-provisioning-aws-helper'
-version '0.1.0'
+version '0.2.0'
 source_url       'https://github.com/irvingpop/chef-provisioning-aws-helper'
 issues_url       'https://github.com/irvingpop/chef-provisioning-aws-helper/issues'
 
